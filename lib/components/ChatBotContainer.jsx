20ca7e21f4acb5577a6714eaff24e65bf9349a69
rachelhs <rachelsmith400@hotmail.co.uk>
2020-07-07 14:09:09 +0100

    background black

--- lib/components/ChatBotContainer.jsx
+++ lib/components/ChatBotContainer.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import defaultTheme from '../theme';
 
 const ChatBotContainer = styled.div`
-  background: ${({ theme }) => theme.background};
+  background: #000000;
   border-radius: 10px;
   box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.15);
   font-family: ${({ theme }) => theme.fontFamily};
